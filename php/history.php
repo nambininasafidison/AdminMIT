2d65f47b162d307b07c995044c795b6a4ba0b850
TsantaMIT <atnastsanta@gmail.com>
2024-03-30 00:44:30 -1200

    Directive

---
+++ php/history.php
@@ -0,0 +1,33 @@
+<?php
+    session_start();
+    include 'main.php';
+
+    $postdata=file_get_contents("php://input");
+    if(isset($postdata) && !empty($postdata)){
+        $data=json_decode($postdata,true);
+        $GLOBAL["value"]=array();
+        $GLOBAL["value"]=return_history($data);
+        header('Content-Type: application/json');    
+        echo json_encode($GLOBAL["value"]);
+    }
+    else{
+        http_response_code(400);
+        header('Content-Type: application/json');
+        echo json_encode(["Error" => "Aucune donnee recue."]);
+    }
+
+    
+function return_history($requete){
+    $list=array();
+    $connect = mysqli_connect("localhost","root","root")or die("Impossible d'ouvrir le fichier.");
+    mysqli_select_db($connect,"mit");
+    $result=mysqli_query($connect,$requete);
+    foreach($result as $r){
+        $list[]=array($r['date'],$r['action']);
+    }
+    mysqli_close($connect);
+    return $list;
+}
+    
+
+?>
