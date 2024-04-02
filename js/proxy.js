f5d9fcb4b4b8b291765d7f9b2d7a2e7e605e0028
NiainaTR <tsantaniainaraherison@gmail.com>
2024-04-02 18:21:55 +0300

    clean proxy

--- js/proxy.js
+++ js/proxy.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const port = document.querySelector('.port #port');
 const maxCache = document.querySelector('.input #max');
 const minCache = document.querySelector('.input #min');
@@ -6,23 +5,12 @@ const timeMaxCache = document.querySelector('.input #cachemin');
 const journalFormat = document.querySelector('.input #sensR');
 const hostname = document.querySelector('.input #host');
 
-=======
-const port = document.querySelector(".port #port");
-const maxCache = document.querySelector(".input #max");
-const minCache = document.querySelector(".input #min");
-const timeMaxCache = document.querySelector(".input #cachemin");
-const journalFormat = document.querySelector(".input #sensR");
-const hostname = document.querySelector(".input #host");
->>>>>>> faad2f3bc5d22f03b7fbe3732a9b4fd04062dd17
 
 const selectElement = document.getElementById("sensL");
 const optionsContainer = document.querySelector(".option");
 
-<<<<<<< HEAD
 
 
-=======
->>>>>>> faad2f3bc5d22f03b7fbe3732a9b4fd04062dd17
 function updateOptions(selectedValue) {
   let htmlContent = "";
 
@@ -44,30 +32,18 @@ function updateOptions(selectedValue) {
         <label for="fileCount">Nombre de fichier</label>
         <input type="number" id="fileCount" name="file" placeholder="256" min="0">
       </div>`;
-<<<<<<< HEAD
   
       optionsContainer.innerHTML = htmlContent;
     } else {
-=======
-
-    optionsContainer.innerHTML = htmlContent;
-  } else {
->>>>>>> faad2f3bc5d22f03b7fbe3732a9b4fd04062dd17
     htmlContent = `
       <div class="input">
         <label for="memorySize">Taille du mémoire:</label>
         <input type="number" id="memorySize" name="stck" placeholder="9999 MB" min="0">
       </div>`;
-<<<<<<< HEAD
       optionsContainer.innerHTML = htmlContent;
     
     }
   
-=======
-    optionsContainer.innerHTML = htmlContent;
-  }
-
->>>>>>> faad2f3bc5d22f03b7fbe3732a9b4fd04062dd17
   optionsContainer.style.display = "block";
 }
 
